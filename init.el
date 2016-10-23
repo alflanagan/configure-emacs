d6a9e6c39767354ca7a0cba1b921dae588c1d3bb
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2016-10-22 20:30:26 -0400

    Load everything in after-init-hook

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
         ("marmalade"   . "https://marmalade-repo.org/packages/")
         ("elpy" . "https://jorgenschaefer.github.io/packages/")
 	))
+
 (setq package-archive-priorities
       '(("melpa-stable" . 20)
         ("marmalade" . 20)
@@ -20,7 +21,12 @@
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
-(setq package-selected-packages '(use-package))
+;; add an extra directory to load path, so .emacs.d doesn't get even
+;; more crowded.
+(add-to-list 'load-path (expand-file-name "src" user-emacs-directory))
+;; (add-to-list 'load-path (expand-file-name "use-package" user-emacs-directory))
+
+;; (setq package-selected-packages '(use-package))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -30,10 +36,12 @@
 
 (package-refresh-contents t)
 
-;; add an extra directory to load path, so .emacs.d doesn't get even
-;; more crowded.
-(add-to-list 'load-path (expand-file-name "src" user-emacs-directory))
+(defun load-everything ()
+  "Loads and executes =configure-emacs.org= file."
+  (require 'use-package)
+  (require 'org)
 
-(require 'org)
-(org-babel-load-file
- (expand-file-name "configure-emacs.org" user-emacs-directory))
+  (org-babel-load-file
+   (expand-file-name "configure-emacs.org" user-emacs-directory)))
+
+(add-hook 'after-init-hook 'load-everything)
