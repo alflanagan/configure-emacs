bf242a13902139b08c7a42b155a09a096da509ab
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2016-11-01 13:15:34 -0400

    package-archive-priorities considered harmful

--- init.el
+++ init.el
@@ -11,13 +11,13 @@
         ("elpy" . "https://jorgenschaefer.github.io/packages/")
 	))
 
-(setq package-archive-priorities
-      '(("melpa-stable" . 20)
-        ("marmalade" . 20)
-        ("org" . 20)
-        ("elpy" . 20)
-        ("gnu" . 10)
-        ("melpa" . 0)))
+;; (setq package-archive-priorities
+;;       '(("melpa-stable" . 20)
+;;         ("marmalade" . 20)
+;;         ("org" . 20)
+;;         ("elpy" . 20)
+;;         ("gnu" . 10)
+;;         ("melpa" . 0)))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
